71c72d3baceb8b1ec9ea275c69fa09bc85313d7a
Phutondog <ericmphu@gmail.com>
2022-07-28 15:42:55 -0400

    added animations

--- eric-website/personal-web/src/app/landing-page/landing-nav/landing-nav.component.ts
+++ eric-website/personal-web/src/app/landing-page/landing-nav/landing-nav.component.ts
@@ -10,6 +10,31 @@ export class LandingNavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.clearActive()
+  }
+
+  clearActive(){
+    const elems = document.querySelectorAll('.bar');
+    elems.forEach(el => {
+      el.classList.remove("active");
+    })
+  }
+
+  onButtonGroupClick($event: any) {
+    let clickedElement = $event.target || $event.srcElement;
+
+    if (clickedElement.nodeName === "BUTTON") {
+
+      let isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector(".active");
+      // if a Button already has Class: .active
+      if (isCertainButtonAlreadyActive) {
+        isCertainButtonAlreadyActive.classList.remove("active");
+      }
+      this.clearActive()
+
+      clickedElement.className += " active";
+    }
+
   }
 
 }
