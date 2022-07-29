d7cfeaae82a782e05638687665176940613a933f
Phutondog <ericmphu@gmail.com>
2022-07-29 17:00:29 -0400

    added more components

--- eric-website/personal-web/src/app/app.module.ts
+++ eric-website/personal-web/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 import { LandingNavComponent } from './landing-page/landing-nav/landing-nav.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { StackComponent } from './pages/stack/stack.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     NavBarComponent,
-    LandingNavComponent
+    LandingNavComponent,
+    AboutComponent,
+    ProjectsComponent,
+    StackComponent
   ],
   imports: [
     BrowserModule,
--- eric-website/personal-web/src/app/common/nav-bar/nav-bar.component.ts
+++ eric-website/personal-web/src/app/common/nav-bar/nav-bar.component.ts
@@ -10,6 +10,38 @@ export class NavBarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    window.setTimeout(function () {
+      document.getElementById('preload1')!.style.visibility = 'visible';
+      document.getElementById('preload2')!.style.visibility = 'visible';
+      document.getElementById('preload3')!.style.visibility = 'visible';
+
+    }, 2500);
   }
+
+  toggleAllIcons(){
+    const elems = document.querySelectorAll('.icon');
+    elems.forEach(el => {
+      el.classList.toggle('active');
+    })
+  }
+
+
+  hamClick($event: any) {
+    let clickedElement = document.querySelector('.ham');
+      // if a Button already has Class: .active
+      // if (isCertainButtonAlreadyActive) {
+      //   this.toggleAllIcons()
+      //   isCertainButtonAlreadyActive.classList.remove("active");
+      // } else{
+    this.toggleAllIcons();
+
+    console.log(clickedElement)
+
+
+        clickedElement!.classList.toggle('active');
+      }
+    // }
+
+  
 
 }
